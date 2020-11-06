19b16e7d3fb57a037fc74baa49dbc954771240fb
mn <jostenriop@gmail.com>
2020-11-05 19:01:00 -0600

    Added models folder

--- API/app.js
+++ API/app.js
@@ -3,11 +3,22 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
 const cors = require('cors');
+require('dotenv/config');
 
 mongoose.connect(process.env.DB_CONNECTION,
     {useUnifiedTopology: true, useNewUrlParser: true},
     () => {console.log('connected to DB')}
 );
+
+//Middleware
+app.use(cors());
+
+app.use(
+    express.urlencoded({
+      extended: true
+    })
+  );
+app.use(express.json());
 
 
 
