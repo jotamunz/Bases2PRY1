700e57d6a0c4fe83213df1b4ab2d82fb261f201b
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-07 14:30:20 -0600

    Font awesome installation and admin dashboard component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'user/dashboard', component: UserDashboardComponent },
+  { path: 'admin/dashboard', component: AdminDashboardComponent },
 ];
 
 @NgModule({
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 import { AuthService } from './services/auth.service';
 
@@ -15,8 +17,9 @@ import { AuthService } from './services/auth.service';
     NavbarComponent,
     LoginComponent,
     UserDashboardComponent,
+    AdminDashboardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FlashMessagesModule.forRoot()],
   providers: [AuthService],
   bootstrap: [AppComponent],
 })
