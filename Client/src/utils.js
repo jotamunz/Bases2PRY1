2b92827b5581b1fb08bfb9532877d53fdf2de081
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-08 00:38:31 -0600

    navbar and side nav functionality

--- Client/src/utils.js
+++ Client/src/utils.js
@@ -1,4 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   let elems = document.querySelectorAll(".sidenav");
-//   let instances = M.Sidenav.init(elems);
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  let elems = document.querySelectorAll(".sidenav");
+  let instances = M.Sidenav.init(elems);
+});
