0f03b2a6650ddc92aba24ec6885de31703358e38
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-08 23:21:54 -0600

    Side navigation

--- Client/src/utils.js
+++ Client/src/utils.js
@@ -1,4 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+function initializeSideNav() {
+  // Initialize sidenav
   let elems = document.querySelectorAll(".sidenav");
-  let instances = M.Sidenav.init(elems);
-});
+  M.Sidenav.init(elems);
+  document
+    .querySelector(".close-sidenav")
+    .addEventListener("click", closeSideNav);
+}
+
+function closeSideNav() {
+  $(".sidenav").sidenav("close");
+}
+
+function loadEventListeners() {
+  document.addEventListener("DOMContentLoaded", initializeSideNav);
+}
+
+loadEventListeners();
