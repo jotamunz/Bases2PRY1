e280dd1864640489251dbf44879ae83a64d95813
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-10 20:51:34 -0600

    navbar authentication functionality

--- Client/src/app/components/layout/navbar/navbar.component.ts
+++ Client/src/app/components/layout/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+
+import { User } from '../../../models/User';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private flashMessagesService: FlashMessagesService,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {}
+
+  /**
+   * Returns the user to the login page
+   */
+  public onLogoutClick(): void {
+    this.authService.logout();
+    this.flashMessagesService.show("You've been logged out", {
+      cssClass: 'alert success-alert',
+    });
+    this.router.navigateByUrl('/');
+  }
+
+  public isUserAuthenticated(): boolean {
+    return this.authService.getIsAuthenticated();
+  }
+
+  public isAdmin(): boolean {
+    return this.authService.getCurrentUser().isAdmin;
+  }
+
+  public getCurrentUser(): User {
+    return this.authService.getCurrentUser();
+  }
 }
