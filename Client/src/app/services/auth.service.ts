209b9257e9bc5b508519420c1ff4f3adfd2748dc
Alegarciy <ale.garciy@gmail.com>
2020-11-10 22:17:03 -0600

    Login interface
    Finished login interface without the authService. This is functioning interface in which user can login with their username and password.

--- Client/src/app/services/auth.service.ts
+++ Client/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getUser() {
+    return [
+      {
+        id: '5',
+        username: 'Alejandro',
+        password: '1234',
+      },
+    ];
+  }
 }
--- Client/src/utils.js
+++
@@ -1,4 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   let elems = document.querySelectorAll(".sidenav");
-//   let instances = M.Sidenav.init(elems);
-// });
