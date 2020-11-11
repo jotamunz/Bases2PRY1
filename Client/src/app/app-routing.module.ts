65b77e01b391e5537b9daf7f3ee93fda34b02abc
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-10 19:32:32 -0600

    Auth user guard implemented

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -5,9 +5,15 @@ import { LoginComponent } from './components/login/login.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
+import { AuthUserGuard } from './guards/authuser.guard';
+
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
-  { path: 'user/dashboard', component: UserDashboardComponent },
+  {
+    path: 'user/dashboard',
+    component: UserDashboardComponent,
+    canActivate: [AuthUserGuard],
+  },
   { path: 'admin/dashboard', component: AdminDashboardComponent },
 ];
 
