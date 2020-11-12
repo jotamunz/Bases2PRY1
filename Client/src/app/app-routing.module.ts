e2caa0eb477e70d4e23e0b0fe61a6812e1c10e3e
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-12 11:52:25 -0600

    routing for new components setup

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { LoginComponent } from './components/login/login.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
+import { UsersComponent } from './components/users/users.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { AssignSchemeToUserComponent } from './components/assign-scheme-to-user/assign-scheme-to-user.component';
 
 import { AuthUserGuard } from './guards/authuser.guard';
 import { AuthAdminGuard } from './guards/authadmin.guard';
@@ -24,6 +27,21 @@ const routes: Routes = [
   {
     path: 'admin/users/register',
     component: UserRegistrationComponent,
+    canActivate: [AuthAdminGuard],
+  },
+  {
+    path: 'admin/users',
+    component: UsersComponent,
+    canActivate: [AuthAdminGuard],
+  },
+  {
+    path: 'admin/:userId/edit',
+    component: EditUserComponent,
+    canActivate: [AuthAdminGuard],
+  },
+  {
+    path: 'admin/schemes/assign',
+    component: AssignSchemeToUserComponent,
     canActivate: [AuthAdminGuard],
   },
 ];
