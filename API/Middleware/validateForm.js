d7b1ea2ec17a4396f943511bd04e6b43cc297398
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2020-11-15 15:27:11 -0600

    added get all info for specific form

---
+++ API/Middleware/validateForm.js
@@ -0,0 +1,25 @@
+const Scheme = require('../models/Scheme');
+
+module.exports = async function validateForm(req, res, next) {
+	const scheme = await Scheme.findOne(
+		{ name: req.body.schemeName },
+		{ _id: 0, fields: 1 }
+	);
+	if (scheme == null) {
+		res.status(400).json({ message: 'Specified scheme not found' });
+		return;
+	}
+	for (let key in req.body.responses) {
+		if (
+			req.body.responses.hasOwnProperty(key) &&
+			scheme.fields.hasOwnProperty(key)
+		) {
+			value = req.body.responses[key];
+			field = scheme.fields[key];
+			console.log(value);
+			console.log(field);
+		}
+	}
+	//TODO
+	//next();
+};
