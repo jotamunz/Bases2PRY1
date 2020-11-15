3ac2a1d99641fa6c0c4f639a2e6a0b9273e56377
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2020-11-14 19:58:04 -0600

    Added get names for users history and pending, get  schemes for user

--- API/Routes/forms.js
+++ API/Routes/forms.js
@@ -7,21 +7,100 @@ const Scheme = require('../models/Scheme');
 const router = express.Router();
 
 /*GETS*/
-// Gets all forms
-router.get('/', async (req, res) => {
+
+// GET PENDNG FORMS BY USERNAME
+// I: /userUsername
+// O: all pending forms scheme names and dates sorted
+// E: 408, 401, 400
+router.get('/pending/:userUsername', verifyToken, async (req, res) => {
 	try {
-		const form = await Form.find();
-		res.json(form);
+		const user = await User.findOne(
+			{ username: req.params.userUsername },
+			{ _id: 1 }
+		);
+		if (user == null) {
+			res.status(400).json({ message: 'Specified user not found' });
+			return;
+		}
+		const forms = await Form.find(
+			{ userId: user._id, status: 0 },
+			{ _id: 0, schemeId: 1, creationDate: 1 }
+		).sort({ creationDate: 0 });
+		let formsWithNames = [];
+		for (let key in forms) {
+			if (forms.hasOwnProperty(key)) {
+				form = forms[key];
+				let schemeName = await Scheme.findOne(
+					{ _id: form.schemeId },
+					{ _id: 0, name: 1 }
+				);
+				if (schemeName == null) {
+					res.status(400).json({ message: 'Specified scheme not found' });
+					return;
+				}
+				formsWithNames.push({
+					schemeName: schemeName.name,
+					creationDate: form.creationDate
+				});
+			}
+		}
+		formsWithNames.sort(function (a, b) {
+			return a.schemeName < b.schemeName
+				? -1
+				: a.schemeName > b.schemeName
+				? 1
+				: 0;
+		});
+		res.json(formsWithNames);
 	} catch (error) {
 		res.status(408).json({ message: error });
 	}
 });
 
-// Gets form by Id
-router.get('/:formId', async (req, res) => {
+// GET HISTORY FORMS BY USERNAME
+// I: /userUsername
+// O: all approved and rejected forms scheme names and dates sorted
+// E: 408, 401, 400
+router.get('/history/:userUsername', verifyToken, async (req, res) => {
 	try {
-		const form = await Form.findById(req.params.formId);
-		res.json(form);
+		const user = await User.findOne(
+			{ username: req.params.userUsername },
+			{ _id: 1 }
+		);
+		if (user == null) {
+			res.status(400).json({ message: 'Specified user not found' });
+			return;
+		}
+		const forms = await Form.find(
+			{ userId: user._id, $or: [{ status: 1 }, { status: 2 }] },
+			{ _id: 0, schemeId: 1, creationDate: 1 }
+		).sort({ creationDate: 0 });
+		let formsWithNames = [];
+		for (let key in forms) {
+			if (forms.hasOwnProperty(key)) {
+				form = forms[key];
+				let schemeName = await Scheme.findOne(
+					{ _id: form.schemeId },
+					{ _id: 0, name: 1 }
+				);
+				if (schemeName == null) {
+					res.status(400).json({ message: 'Specified scheme not found' });
+					return;
+				}
+				formsWithNames.push({
+					schemeName: schemeName.name,
+					creationDate: form.creationDate
+				});
+			}
+		}
+		formsWithNames.sort(function (a, b) {
+			return a.schemeName < b.schemeName
+				? -1
+				: a.schemeName > b.schemeName
+				? 1
+				: 0;
+		});
+		res.json(formsWithNames);
 	} catch (error) {
 		res.status(408).json({ message: error });
 	}
@@ -65,7 +144,7 @@ router.post('/', verifyToken, async (req, res) => {
 			responses: req.body.responses
 		});
 		const savedForm = await form.save();
-		res.json({ name: savedForm.creationDate });
+		res.json({ date: savedForm.creationDate });
 	} catch (error) {
 		res.status(408).json({ message: error });
 	}
