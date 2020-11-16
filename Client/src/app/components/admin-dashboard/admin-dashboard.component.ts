542d00af61ece7c76c0b6132632da3fd98a4790f
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-16 17:52:15 -0600

    progress bars completed

--- Client/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ Client/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -24,6 +24,8 @@ export class AdminDashboardComponent implements OnInit {
     this.formService
       .getPendingFormForUser(username)
       .subscribe((schemes: any[]) => {
+        console.log('Current schemes', schemes);
+
         // Map to items in form
         schemes.forEach((scheme) => {
           const { progress } = scheme;
@@ -40,8 +42,28 @@ export class AdminDashboardComponent implements OnInit {
               this.calculateApprovalProgress(
                 progress,
                 scheme.requiredTotalApprovals
-              ).toString() + '%';
+              )
+                .toFixed(1)
+                .toString() + '%';
           }
+
+          // Get required rejecions
+          scheme.requiredTotalRejections = this.calculateRequiredRejections(
+            progress
+          );
+          // Check for required rejections = 0
+          if (scheme.requiredTotalRejections == 0) {
+            scheme.rejectionProgress = '0%';
+          } else {
+            scheme.rejectionProgress =
+              this.calculateRejectionProgress(
+                progress,
+                scheme.requiredTotalRejections
+              )
+                .toFixed(1)
+                .toString() + '%';
+          }
+
           // Add to pending documents
           this.pendingDocuments.push(scheme);
         });
@@ -64,8 +86,22 @@ export class AdminDashboardComponent implements OnInit {
   }
 
   /**
+   * Calculates the total required rejections for a document
+   * @param progress The progress of each approval route
+   */
+  private calculateRequiredRejections(progress: any[]): number {
+    let totalRejections = 0;
+    progress.forEach((progress) => {
+      const { requiredRejections } = progress;
+      totalRejections += requiredRejections;
+    });
+    return totalRejections;
+  }
+
+  /**
    * Calculates the current approvals for a document
    * @param progress The progress of each approval route
+   * @param requiredApprovals The required approvals of a route
    */
   private calculateApprovalProgress(
     progress: any[],
@@ -78,5 +114,23 @@ export class AdminDashboardComponent implements OnInit {
     });
 
     return (currentTotalApprovals / requiredApprovals) * 100;
+  }
+
+  /**
+   * Calculates the rejection progress of a document
+   * @param progress The progress of each approval route
+   * @param requiredRejections The required rejections for a route
+   */
+  private calculateRejectionProgress(
+    progress: any[],
+    requiredRejections: number
+  ): number {
+    let currentTotalRejections = 0;
+    progress.forEach((progress) => {
+      const { currentRejections } = progress;
+      currentTotalRejections += currentRejections;
+    });
+
+    return (currentTotalRejections / requiredRejections) * 100;
   }
 }
