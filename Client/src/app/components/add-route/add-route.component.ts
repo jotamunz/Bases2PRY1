9b33416afec8ae398772c6d7798b70db4e137d34
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-18 20:23:37 -0600

    create approval route ready

--- Client/src/app/components/add-route/add-route.component.ts
+++ Client/src/app/components/add-route/add-route.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { SchemeService } from '../../services/scheme.service';
 import { UserService } from '../../services/user.service';
 import { AuthService } from '../../services/auth.service';
+import { RoutePreviewService } from '../../services/Route-Services/route-preview.service';
 
 import { Scheme } from '../../models/Scheme';
 import { User } from '../../models/User';
@@ -15,10 +17,11 @@ import { ApprovalRoute } from '../../models/ApprovalRoute';
 })
 export class AddRouteComponent implements OnInit {
   public schemes: Scheme[] = [];
-  public authors: User[] = [];
-  public approvers: User[] = [];
+  public authorItems: any[];
+  public approverItems: any[];
   public newRoute: ApprovalRoute = {
     name: '',
+    schemeName: '',
     requiredApprovals: 0,
     requiredRejections: 0,
     authors: [],
@@ -29,30 +32,125 @@ export class AddRouteComponent implements OnInit {
     private flashMessagesService: FlashMessagesService,
     private schemeService: SchemeService,
     private userService: UserService,
-    private authService: AuthService
+    private authService: AuthService,
+    private routeService: RoutePreviewService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.loadFormInformation();
-  }
-
-  /**
-   * Loads all the schemes
-   */
-  public loadFormInformation(): void {
     // Get all schemes
     this.schemeService.getAllSchemes().subscribe((schemes: Scheme[]) => {
       this.schemes = schemes;
     });
     // Get all authors
     this.userService.getAllUsers().subscribe((users: User[]) => {
-      this.authors = users.filter(
+      let userItems = users.filter(
         (user) => user.username !== this.authService.getCurrentUser().username
       );
+      userItems = userItems.map((item) => ({
+        username: item.username,
+        checked: false,
+      }));
+      this.authorItems = userItems;
     });
     // Get all admins
     this.userService.getAdmins().subscribe((admins: User[]) => {
-      this.approvers = admins;
+      let approverItems = admins.map((user) => ({
+        username: user.username,
+        checked: false,
+      }));
+      this.approverItems = approverItems;
     });
+  }
+
+  public onAuthorSelected(username: string): void {
+    this.approverItems.forEach((approver) => {
+      if (approver.username === username) {
+        approver.checked = false;
+      }
+    });
+  }
+
+  public onApproverSelected(username: string): void {
+    this.authorItems.forEach((author) => {
+      if (author.username === username) {
+        author.checked = false;
+      }
+    });
+  }
+
+  public onSchemeSelected(schemeName: string): void {
+    this.newRoute.schemeName = schemeName;
+  }
+
+  /**
+   * Submits the form
+   */
+  public onSubmit(): void {
+    // Field validations
+    if (
+      this.newRoute.name == '' ||
+      this.newRoute.requiredApprovals == null ||
+      this.newRoute.requiredRejections == null ||
+      this.newRoute.schemeName == ''
+    ) {
+      this.flashMessagesService.show('Please fill in the form correctly', {
+        cssClass: 'alert danger-alert',
+      });
+      return;
+    }
+    // Get authors
+    this.authorItems.forEach((author) => {
+      if (author.checked) {
+        this.newRoute.authors.push(author);
+      }
+    });
+    // Get approvers
+    this.approverItems.forEach((approver) => {
+      if (approver.checked) {
+        this.newRoute.approvers.push(approver);
+      }
+    });
+    // Check for approvers and authors
+    if (
+      this.newRoute.authors.length == 0 ||
+      this.newRoute.approvers.length == 0
+    ) {
+      this.flashMessagesService.show(
+        'Please enter approvals or authors to the route',
+        {
+          cssClass: 'alert danger-alert',
+        }
+      );
+      return;
+    }
+    // Check for consistency in rejections and approvals
+    if (
+      this.newRoute.approvers.length < this.newRoute.requiredApprovals ||
+      this.newRoute.approvers.length < this.newRoute.requiredRejections
+    ) {
+      this.flashMessagesService.show(
+        'Please validate your entries for rejections and approvals',
+        {
+          cssClass: 'alert danger-alert',
+        }
+      );
+      return;
+    }
+    // Add route
+    this.routeService.postRoute(this.newRoute).subscribe(
+      (response: any) => {
+        // Show message and redirect
+        this.flashMessagesService.show('Approval route created', {
+          cssClass: 'alert success-alert',
+        });
+        this.router.navigateByUrl('/admin/routePreview');
+      },
+      (err) => {
+        this.flashMessagesService.show(err.error.message, {
+          cssClass: 'alert danger-alert',
+        });
+      }
+    );
   }
 }
