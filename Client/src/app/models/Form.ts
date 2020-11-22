cb1b787da9a70e9758556ee745c3d305723ddfbd
aguilarandress <andres.aguilarmoya2001@gmail.com>
2020-11-22 16:12:59 -0600

    Update de Form.ts

--- Client/src/app/models/Form.ts
+++ Client/src/app/models/Form.ts
@@ -1,7 +1,6 @@
 export interface Form {
-    _id?: string;
-    schemeName? : string;
-    userUsername?: string;
-    responses?:any [];
-    
-  }
+  _id?: string;
+  schemeName?: string;
+  userUsername?: string;
+  responses?: any[];
+}
